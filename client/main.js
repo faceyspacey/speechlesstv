416d6b80e1b20e93e1011a42603f0be540c52895
James Gillmore <james@faceyspacey.com>
2013-03-14 01:19:55 -0400

    test by jg

---
+++ client/main.js
@@ -0,0 +1,19 @@
+Meteor.startup(function(){
+	setTimeout(function() {
+		Session.set('current_video', Videos.findOne({category_id: 1}, {sort: {length: -1}}));
+	}, 500);
+	
+	console.log('james was here');
+});
+
+Accounts.ui.config({
+  requestPermissions: {
+    facebook: ['user_likes'],
+    github: ['user', 'repo']
+  },
+  requestOfflineToken: {
+    google: true
+  },
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
