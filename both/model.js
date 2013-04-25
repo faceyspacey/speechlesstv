bde0c433336de9deb6ce5c4fe4dd555748ad3e93
James Gillmore <james@faceyspacey.com>
2013-04-25 06:10:57 -0700

    fix after emilio television broke due to meteor upgrades

--- both/model.js
+++ both/model.js
@@ -1,10 +1,10 @@
-var categories = {all: 0, comedy: 1, music: 2, documentary: 3, tech: 4, education: 5, news: 6, skate: 7, hot: 8, gym: 9}; 
+categories = {all: 0, comedy: 1, music: 2, documentary: 3, tech: 4, education: 5, news: 6, skate: 7, hot: 8, gym: 9}; 
 
 //stupid hack to diplay stuff on page to admins only
-var EmillsId = 'LuoAzFyHfgDYckgf4',
- 	JamesId = 'HztCMrSSjZkC3dKwD',
-	CarlosId = 'Bev9aC8scyeKFXi7X',
-	admins = [EmillsId, JamesId, CarlosId];
+EmillsId = 'LuoAzFyHfgDYckgf4';
+JamesId = 'HztCMrSSjZkC3dKwD';
+CarlosId = 'Bev9aC8scyeKFXi7X';
+admins = [EmillsId, JamesId, CarlosId];
 	
 Videos = new Meteor.Collection('videos');
 Categories = new Meteor.Collection('categories');
