40201b9f4827e25475cbc3f1fa77f26c51e6bc8b
James Gillmore <james@faceyspacey.com>
2013-12-30 19:38:05 -0800

    prepare facebook config for server

--- server/startup.js
+++ server/startup.js
@@ -9,8 +9,23 @@ Meteor.startup(function () {
 		});
 	}
 	
+	
 	if(Meteor.roles.find().count() === 0) {
 		Roles.createRole('admin');
 		Roles.createRole('user');
 	}
+	
+	
+	 Accounts.loginServiceConfiguration.remove({
+	    service: "facebook"
+	  });
+
+	  Accounts.loginServiceConfiguration.insert({
+	    service: "facebook",
+	    appId: '697368233620325',
+	    secret: 'c8c1c325c950d984b9293fd24b419a0c'
+	  });
 });
+
+
+
