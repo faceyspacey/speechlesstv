b7a2f19f12168e4bd4f94e37d4ff7a50ec2cbacf
James Gillmore <james@faceyspacey.com>
2014-02-18 01:22:19 -0800

    enhanced search results algorithm

--- client/js~/internal_lib/youtube_searcher.js
+++ client/js~/internal_lib/youtube_searcher.js
@@ -1,49 +1,76 @@
 YoutubeSearcher = {
+	maxThumbs: SearchSizes.thumbsPerColumn,
+	queries: {},
+	related: {},
+	
 	query: function(query) {
+		Deps.afterFlush(BackNext.addColumn.bind(BackNext));
+		
+		if(this.queries[query]) return this._store(this.queries[query]);
+		
 		this._execute({
 			part: 'snippet', 
 			type: 'video',
 			q: query
 		});
 	},
-	related: function(youtubeVideoId) {
+	related: function(relatedToVideoId) {
+		Deps.afterFlush(BackNext.addColumn.bind(BackNext));
+		
+		if(this.related[relatedToVideoId]) return this._store(this.related[relatedToVideoId]);
+		
 		this._execute({
 			part: 'snippet', 
 			type: 'video',
-			relatedToVideoId: youtubeVideoId
+			relatedToVideoId: relatedToVideoId
 		});
 	},
-	_execute: function(params) {
-		Deps.afterFlush(BackNext.addColumn.bind(BackNext));
-		
+	_execute: function(params) {	
 		params.videoEmbeddable = true;
-		params.maxResults = SearchSizes.thumbsPerColumn + 5; 
-		gapi.client.youtube.search.list(params).execute(this._store.bind(this));
-	},
-	_store: function(response) {
-		var c = this._newColumn(),
-			maxThumbs = SearchSizes.thumbsPerColumn,
-			thumbsSupplied = 0,
-			currentVideos = Videos.find({_local: true}).map(function(video) {
-				return video.youtube_id;
-			});
-		
-		_.each(response.items, function(video, index) {
-			if(thumbsSupplied < maxThumbs && currentVideos.indexOf(video.id.videoId) === -1) {
-				var v = new VideoModel;
-				v.youtube_id = video.id.videoId;
-				v.title = video.snippet.title;
-				v.published_at = moment(video.snippet.publishedAt).toDate();
-				v.description = video.snippet.description;
-				v.column_index = c.index;
-				v.index = index;
-				v.length = '00:00';
-				v.created_at = moment().toDate();
-				v.store();
-				
-				thumbsSupplied++;
-			}
+		params.maxResults = 30; 
+		gapi.client.youtube.search.list(params).execute(function(response) {
+			this._store(response.items, params.q, params.relatedToVideoId);
 		}.bind(this));
+	},
+	_store: function(newVideos, q, relatedToVideoId) {	
+		var column = this._newColumn(),
+			videosAdded = [],
+			currentVideos = Videos.find({_local: true}).map(function(video) { return video.youtube_id; });
+		
+		this._skipDuplicates(newVideos, column, videosAdded, function(video) {
+			return currentVideos.indexOf(video.id.videoId) === -1;
+		});
+		
+		//fallback to duplicates, but not if in current column
+		if(videosAdded.length < this.maxThumbs) this._skipDuplicates(_.shuffle(newVideos), column, videosAdded, function(video) {
+			return videosAdded.indexOf(video.id.videoId) === -1;
+		});
+		
+		this._storeQuery(q, relatedToVideoId, newVideos);
+	},
+	_storeQuery: function(q, relatedToVideoId, newVideos) {
+		if(q) this.queries[q] = newVideos;
+		if(relatedToVideoId) this.related[relatedToVideoId] = newVideos;
+	},
+	_skipDuplicates: function(videos, column, videosAdded, isTrueFunc) {
+		_.each(videos, function(video, index) {
+			if(videosAdded.length >= this.maxThumbs) return;
+			if(!isTrueFunc || isTrueFunc.call(this, video)) this._addVideo(video, column, index, videosAdded);
+		}.bind(this));
+	},
+	_addVideo: function(video, column, index, videosAdded) {
+		var v = new VideoModel;
+		v.youtube_id = video.id.videoId;
+		v.title = video.snippet.title;
+		v.published_at = moment(video.snippet.publishedAt).toDate();
+		v.description = video.snippet.description;
+		v.column_index = column.index;
+		v.index = index;
+		v.length = '00:00';
+		v.created_at = moment().toDate();
+		v.store();
+		
+		videosAdded.push(v.youtube_id);
 	},
 	_newColumn: function() {
 		var c = new ColumnModel;
